1ce09043bb0303fa50f2addd6790446e62411dd3
Christian Moll <christian.moll@tudor.lu>
2015-02-11 15:28:40 +0100

    most needed g-codes

---
+++ src/de/lazyzero/timelapse2gcode/Gcode.java
@@ -0,0 +1,21 @@
+package de.lazyzero.timelapse2gcode;
+
+public class Gcode {
+	
+	public static final String G0 = "G0";
+	public static final String G1 = "G1";
+	public static final String G4 = "G4";
+	public static final String G4_P = "G4 P";
+	
+	public static final String G90 = "G0";
+	public static final String G91 = "G0";
+	
+	public static final String M3 = "M3";
+	public static final String M4 = "M4";
+	public static final String M5 = "M5";
+	
+	public static final String M7 = "M7";
+	public static final String M8 = "M8";
+	public static final String M9 = "M9";
+	
+}
